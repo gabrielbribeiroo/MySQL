124fce3003b7e008f2b1d721a17cbe3002f703b5
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-09-29 17:12:56 -0300

    rename table

--- TEACHER.sql
+++ TEACHER.sql
@@ -2,7 +2,7 @@ create database teacher
 default character set utf8mb4
 default collate utf8mb4_general_ci;
 
-create table `finance` (
+create table if not exists finance_year (
 id int not null auto_increment,
 `date` date,
 reference varchar(15) not null,
@@ -14,7 +14,7 @@ bank_destination varchar(15),
 primary key(id)
 ) default charset = utf8mb4;
 
-create table if not exists classes_month (
+create table if not exists classes_month_year (
 id int not null,
 `date` date,
 student varchar(30) not null,
