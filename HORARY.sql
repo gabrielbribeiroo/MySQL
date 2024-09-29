1bc5e7fbf9566c3b600e961b029b3f9b9b4dbcc8
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2024-09-29 11:52:17 -0300

    Update HORARY.sql

--- HORARY.sql
+++ HORARY.sql
@@ -1,19 +1,15 @@
-create database cronograma
+create database timeline
 default character set utf8mb4
 default collate utf8mb4_general_ci;
 
-use cronograma;
-
-create table horario (
+create table if not exists horary (
 id int not null auto_increment,
-inicio varchar(5) NOT NULL,
-fim varchar(5) NOT NULL,
-segunda text,
-terça text,
-quarta text,
-quinta text,
-sexta text,
+`start` varchar(5) NOT NULL,
+`end` varchar(5) NOT NULL,
+monday text,
+tuesday text,
+wednesdsay text,
+thursday text,
+friday text,
 primary key (id)
 ) default charset = utf8mb4;
-
-select * from horario;
