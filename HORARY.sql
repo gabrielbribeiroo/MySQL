aae5f0d7517b5d536a4ef1c15a30e7990a50cc31
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-02-21 22:49:51 -0300

    Update HORARY.sql

--- HORARY.sql
+++ HORARY.sql
@@ -3,13 +3,13 @@ default character set utf8mb4
 default collate utf8mb4_general_ci;
 
 create table if not exists horary (
-id int not null auto_increment,
-`start` varchar(5) NOT NULL,
-`end` varchar(5) NOT NULL,
-monday text,
-tuesday text,
-wednesdsay text,
-thursday text,
-friday text,
-primary key (id)
+  id int not null auto_increment,
+  `start` varchar(5) NOT NULL,
+  `end` varchar(5) NOT NULL,
+  monday text,
+  tuesday text,
+  wednesdsay text,
+  thursday text,
+  friday text,
+  primary key (id)
 ) default charset = utf8mb4;
