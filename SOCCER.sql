416e9a263ed0007b1f6d370cb644c2f3cea7ad5c
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-02-21 23:07:37 -0300

    change table for players

--- SOCCER.sql
+++ SOCCER.sql
@@ -2,21 +2,27 @@ create database soccer
 default character set utf8mb4
 default collate utf8mb4_general_ci;
 
+use soccer;
+
+-- table for players -- 
 create table if not exists player (
   id int not null auto_increment,
   name varchar(50) not null,
-  team varchar(30),
-  position varchar(15) not null,
+  team_id int,
+  position_id int not null,
   `start` date,
   `end` date,
-  `value` numeric,
-  contract varchar(15) default 'Buy',
+  `value` decimal(10, 2),
+  contract int default 1,
   birth date check (birth <= CURDATE()),
   height decimal(3,2),
   nationality varchar(20) default 'Brazil',
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  primary key(id)
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
+  primary key(id),
+  foreign key (team_id) references team(id) on delete set null on update cascade,
+  foreign key (position_id) references position(id) on delete restrict on update cascade,
+  foreign key (contract) references contract on delete restrict on update cascade
 ) default charset = utf8mb4;
 
 create table if not exists team (
