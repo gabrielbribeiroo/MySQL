d58cb7c368822ab30ae309d42f242b03433d55c2
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-02-22 11:25:02 -0300

    table for time slots

--- HORARY.sql
+++ HORARY.sql
@@ -2,14 +2,14 @@ create database timeline
 default character set utf8mb4
 default collate utf8mb4_general_ci;
 
+use timeline;
+
+-- table for time slots --
 create table if not exists horary (
   id int not null auto_increment,
-  `start` varchar(5) NOT NULL,
-  `end` varchar(5) NOT NULL,
-  monday text,
-  tuesday text,
-  wednesdsay text,
-  thursday text,
-  friday text,
+  start_time time NOT NULL,
+  end_time time NOT NULL,
+  created_at timestamp default current_timestamp,
+  updated_at TIMESTAMP default current_timestamp on update current_timestamp,
   primary key (id)
 ) default charset = utf8mb4;
