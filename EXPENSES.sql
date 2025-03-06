b2bac21f36ce620009b1a320429d1ad78318150b
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-03-06 12:48:02 -0300

    users table

--- EXPENSES.sql
+++ EXPENSES.sql
@@ -1,1 +1,14 @@
+-- Create a database for expense control
+CREATE DATABASE IF NOT EXISTS expenses
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
 
+-- Users table
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL UNIQUE,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password_hash VARCHAR(255) NOT NULL, -- Store encrypted password
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8mb4;
