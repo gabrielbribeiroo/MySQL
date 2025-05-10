a640f912a865da5faa0a0200fa47e149a9d15868
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-05-10 13:49:45 -0300

    Table of projects that group tasks

--- TASK.sql
+++ TASK.sql
@@ -13,3 +13,14 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 ) DEFAULT CHARSET = utf8mb4;
+
+-- Table of projects that group tasks
+CREATE TABLE IF NOT EXISTS projects (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  description TEXT,
+  start_date DATE,
+  end_date DATE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8mb4;
