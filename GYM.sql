9931ba15db4b45f537ded84e9045f94a3ccd8dd1
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-05-12 09:43:50 -0300

    Table to store clients

--- GYM.sql
+++ GYM.sql
@@ -4,3 +4,15 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE gym;
+
+-- Table to store clients
+CREATE TABLE IF NOT EXISTS clients (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  birth_date DATE,
+  gender ENUM('Male', 'Female', 'Other'),
+  email VARCHAR(100),
+  phone VARCHAR(20),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) DEFAULT CHARSET = utf8mb4;
