40f91a04b1d411a2742e4f3c84deca9d175c6d8c
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-07-28 08:44:30 -0300

    add areas table to database

--- education/SCIENCE_FAIR.sql
+++ education/SCIENCE_FAIR.sql
@@ -3,3 +3,9 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE science_fair;
+
+CREATE TABLE science_areas (
+    area_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT
+);
