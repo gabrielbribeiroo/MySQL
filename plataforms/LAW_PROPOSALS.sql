cbfab7bec73799b16e56f0fd9fc42f10310a5a1c
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-08-01 12:13:02 -0300

    create database

---
+++ plataforms/LAW_PROPOSALS.sql
@@ -0,0 +1,7 @@
+-- Database for tracking legislative proposals, their authors, topics, stages, and votes
+-- Create database
+CREATE DATABASE IF NOT EXISTS law_proposals
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+USE law_proposals;
