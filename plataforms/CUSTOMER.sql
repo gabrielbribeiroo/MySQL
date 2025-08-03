acec07884864ac40f07a67bbbbb80fb1b189e66d
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-08-03 15:16:30 -0300

    add categories table to database

--- plataforms/CUSTOMER.sql
+++ plataforms/CUSTOMER.sql
@@ -4,3 +4,8 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE customer_support;
+
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
