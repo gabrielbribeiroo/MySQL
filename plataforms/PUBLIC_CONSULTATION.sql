b202997b416253ced4d6c0d08ca077b3dbc63872
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-08-04 10:37:03 -0300

    add consultation_topics table to database

--- plataforms/PUBLIC_CONSULTATION.sql
+++ plataforms/PUBLIC_CONSULTATION.sql
@@ -3,3 +3,12 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE public_consultation;
+
+CREATE TABLE consultation_topics (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    area VARCHAR(100), -- ex: Leis, Obras, Mobilidade, etc.
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
