543153efb3b350f6d771e4b19a45a707f49ac23b
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-08-13 10:17:39 -0300

    add initial database setup for freelancer_marketplace

---
+++ finance/FREELANCER.sql
@@ -0,0 +1,8 @@
+-- Database: freelancer_marketplace
+
+-- Create the main database
+CREATE DATABASE IF NOT EXISTS freelancer_marketplace
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+use freelancer_marketplace;
