d9075ff52308a5d4fcbe583e7b33e61e403a3274
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-08-19 10:00:48 -0300

    add categories table to ecommerce database

--- commerce/ECOMMERCE.sql
+++ commerce/ECOMMERCE.sql
@@ -3,3 +3,9 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE ecommerce;
+
+CREATE TABLE categories (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT
+);
