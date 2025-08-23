a12a98e5acbb8a44e6c588250b8890b6d8e0f26b
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-08-23 11:16:37 -0300

    add menu_categories table to database

--- commerce/RESTAURANT.sql
+++ commerce/RESTAURANT.sql
@@ -4,3 +4,8 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE restaurant_management;
+
+CREATE TABLE menu_categories (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL
+);
