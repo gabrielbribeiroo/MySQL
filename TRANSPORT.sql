1bbd35688893c75f9174bcc87a6adeb054684204
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-09-04 06:14:58 -0300

    Atualizar o TRANSPORT.sql

--- TRANSPORT.sql
+++ TRANSPORT.sql
@@ -1,4 +1,10 @@
 -- Database: public_transport
+CREATE DATABASE IF NOT EXISTS public_transport
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_general_ci;
+
+USE public_transport;
+
 CREATE TABLE lines (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
