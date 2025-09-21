15da9b7be74bdcf3499fc1448650cdde8c8a79bd
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-09-21 18:14:24 -0300

    add categories table to database

--- communication/NEWS_PORTAL.sql
+++ communication/NEWS_PORTAL.sql
@@ -1,5 +1,12 @@
+-- Database: news_portal
 CREATE DATABASE news_portal
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE news_portal;
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    description TEXT
+);
