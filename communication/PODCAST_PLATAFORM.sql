4a0c9952b7cd4bf6342e71dee6742ee1f76f2a83
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-09-23 09:46:42 -0300

    add hosts table to database

--- communication/PODCAST_PLATAFORM.sql
+++ communication/PODCAST_PLATAFORM.sql
@@ -4,3 +4,11 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE podcast_platform;
+
+CREATE TABLE hosts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    bio TEXT,
+    joined_at DATE DEFAULT CURRENT_DATE
+);
