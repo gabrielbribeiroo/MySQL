72d0ae178ce8880fc2463bbf58d1e232f52e75c9
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-10-03 12:09:02 -0300

    add positions table to database

--- business/HR_MANAGEMENT.sql
+++ business/HR_MANAGEMENT.sql
@@ -4,3 +4,10 @@ CREATE DATABASE IF NOT EXISTS hr_management
   DEFAULT COLLATE utf8mb4_general_ci;
 
 USE hr_management;
+
+CREATE TABLE positions (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    department VARCHAR(100),
+    description TEXT
+);
