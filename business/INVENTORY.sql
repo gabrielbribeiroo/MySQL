59deeacff071f4ec8ec3d5b3ee3e11e788b90d3b
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-10-13 13:30:15 -0300

    Inventory auditing and valuation platform

---
+++ business/INVENTORY.sql
@@ -0,0 +1,8 @@
+-- Database: inventory_audit
+-- Inventory auditing and valuation platform
+
+CREATE DATABASE inventory_audit
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_general_ci;
+
+USE inventory_audit;
