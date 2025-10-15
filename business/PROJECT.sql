44505225796a5847556e35cf83ed314e2474535c
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-10-15 10:44:52 -0300

    Corporate project portfolio manager

---
+++ business/PROJECT.sql
@@ -0,0 +1,8 @@
+-- Database: project_portfolio
+-- Corporate project portfolio manager
+
+CREATE DATABASE project_portfolio
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+
+USE project_portfolio;
