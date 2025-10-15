bf581ad1f5479b2d0cebc9b3dae84b12eeed2140
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-10-15 10:46:01 -0300

    add departments table to database

--- business/PROJECT.sql
+++ business/PROJECT.sql
@@ -6,3 +6,11 @@ CREATE DATABASE project_portfolio
     COLLATE utf8mb4_general_ci;
 
 USE project_portfolio;
+
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    manager VARCHAR(120),
+    email VARCHAR(150),
+    phone VARCHAR(40)
+);
