697d862e440e8afe22ee6db2520e2573bd4615ce
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-10-17 09:35:48 -0300

    Appointment scheduling system for consultants, salons, and professionals

---
+++ productivity/APPOINTMENT.sql
@@ -0,0 +1,8 @@
+-- Database: appointment_booking
+-- Appointment scheduling system for consultants, salons, and professionals
+
+CREATE DATABASE appointment_booking
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+    
+USE appointment_booking;
