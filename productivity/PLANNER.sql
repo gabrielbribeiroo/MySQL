51065c00dba8ad7445f7e59deebc8893ed736c05
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-10-19 09:48:05 -0300

    Team scheduling system to allocate resources, shifts, and workload balancing

---
+++ productivity/PLANNER.sql
@@ -0,0 +1,8 @@
+-- Database: team_planner
+-- Team scheduling system to allocate resources, shifts, and workload balancing
+
+CREATE DATABASE team_planner
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+    
+USE team_planner;
