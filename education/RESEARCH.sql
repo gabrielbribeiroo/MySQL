495f01fba59f843fb67560e87e2271e363807d34
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-10-28 12:19:38 -0300

    Repository for academic publications, theses, and research data sharing among institutions

---
+++ education/RESEARCH.sql
@@ -0,0 +1,8 @@
+-- Database: research_repository
+-- Repository for academic publications, theses, and research data sharing among institutions
+
+CREATE DATABASE research_repository
+    DEFAULT CHARACTER SET utf8mb4
+    DEFAULT COLLATE utf8mb4_general_ci;
+
+USE research_repository;
