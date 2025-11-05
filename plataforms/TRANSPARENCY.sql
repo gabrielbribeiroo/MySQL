152a017b7445df928a447953d2d407d826cbf272
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-11-05 20:57:09 -0300

    Public transparency system showing government budgets, expenditures, and contracts

---
+++ plataforms/TRANSPARENCY.sql
@@ -0,0 +1,8 @@
+-- Database: transparency_portal
+-- Public transparency system showing government budgets, expenditures, and contracts
+
+CREATE DATABASE transparency_portal
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+USE transparency_portal;
