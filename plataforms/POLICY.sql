405b2c87ab004fa273b83248507cb2123121ad41
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-11-10 12:04:43 -0300

    Tracker for monitoring implementation and results of public policies across regions

---
+++ plataforms/POLICY.sql
@@ -0,0 +1,8 @@
+-- Database: policy_tracker
+-- Tracker for monitoring implementation and results of public policies across regions
+
+CREATE DATABASE policy_tracker
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+USE policy_tracker;
