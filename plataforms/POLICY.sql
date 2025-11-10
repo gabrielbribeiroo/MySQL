60a3851e8fe0d8884f6dd8c183e3e9f31becf6b7
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-11-10 12:05:09 -0300

    feat: add regions table to database

--- plataforms/POLICY.sql
+++ plataforms/POLICY.sql
@@ -6,3 +6,11 @@ DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_general_ci;
 
 USE policy_tracker;
+
+CREATE TABLE regions (
+    region_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    type ENUM('Municipality', 'State', 'Region', 'Country') DEFAULT 'Municipality',
+    parent_region_id INT,
+    FOREIGN KEY (parent_region_id) REFERENCES regions(region_id)
+);
