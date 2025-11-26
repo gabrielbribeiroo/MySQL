058d75919ebf0b71d654db412d2596ee6d08c810
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-11-26 11:32:46 -0300

    Atualiza script ACADEMIC.sql

---
+++ research/ACADEMIC.sql
@@ -0,0 +1,8 @@
+-- Database: academic_network
+-- Academic collaboration network with profiles, institutions, publications, and joint projects.
+
+CREATE DATABASE academic_network
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+USE academic_network;
