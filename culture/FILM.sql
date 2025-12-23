7f68567c1e398ffbf38672eaed5dad53a4c71276
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2025-12-23 00:26:08 -0300

    Film festival management with submissions, screenings, juries, and awards

---
+++ culture/FILM.sql
@@ -0,0 +1,8 @@
+-- Database: film_festival
+-- Film festival management with submissions, screenings, juries, and awards
+
+CREATE DATABASE film_festival
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_general_ci;
+
+USE film_festival;
