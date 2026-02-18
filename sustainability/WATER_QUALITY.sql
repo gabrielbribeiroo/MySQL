90aaf0526060625c24f55274b6f108c0469fb7b0
Gabriel Ribeiro <gabrielbroliveira@gmail.com>
2026-02-18 11:47:00 -0300

    feat: create a system to monitor water quality parameters

---
+++ sustainability/WATER_QUALITY.sql
@@ -0,0 +1,8 @@
+-- Database: water_quality_monitor
+-- System to monitor water quality parameters across regions and generate environmental reports.
+
+CREATE DATABASE water_quality_monitor
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_general_ci;
+
+USE water_quality_monitor;
